d693817338b87c9e50e7fa8f4e0998d3ebae33ba
hambarcumyannarek <narmenia878@gmail.com>
2024-05-25 08:45:53 +0400

    done

--- xss-csrf/index.js
+++ xss-csrf/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const app = express();
+const crypto = require("crypto")
+const cookieParser = require("cookie-parser");
 
 app.use(express.json())
 .use(express.urlencoded({extended: true}))
 .use(express.static('public'))
+.use(cookieParser())
+.use(async (req, res, next) => {
+    const csrfToken = await crypto.randomBytes(77).toString('hex');
+    res.locals.csrfToken = csrfToken;
+    res.cookie('csrfToken', csrfToken, { httpOnly: true, secure: true, sameSite: 'Strict' });
+    next();
+})
+.use(async (req, res, next) => {
+    const { _csrf } = req.body;
+    if (req.method === 'GET') {
+        console.log('auto');
+        return next();
+    } else if (_csrf === req.cookies.csrfToken && _csrf|| req.cookies.csrfToken) {
+        console.log({
+            html: _csrf,
+            cookie: req.cookies.csrfToken
+        })
+        return next();
+    }
+})
 .set('view-engine', 'ejs');
 
 app.get('/', (req, res) => {
